cce150210f833f0301cba96ce73a62d865bedce3
c90x <e.0xcc3@gmail.com>
2025-11-01 03:01:16 +0100

    feat: add getUserById query with error handling

--- src/routes/app/users/[slug]/user.remote.ts
+++ src/routes/app/users/[slug]/user.remote.ts
@@ -1,10 +1,19 @@
 import { z } from 'zod';
 
-import { command } from '$app/server';
+import { command, query } from '$app/server';
 import { error } from '@sveltejs/kit';
 
 import { withAuthClient } from '@/server/auth';
 import * as sdk from '@/backend/sdk.gen';
+
+export const getUserById = query(z.uuidv4(), async (id) => {
+	const { client } = withAuthClient({ superuser: true });
+	const result = await sdk.getUserById({ client, path: { id } });
+
+	if (!!result.data) return result.data;
+
+	error(result.response.status, result.error.detail?.at(0)?.msg ?? 'Unknown Error');
+});
 
 export const editUser = command(
 	z.object({
