18a14ba7323d90c7c0fa2aed74c656992bad4eee
c90x <e.0xcc3@gmail.com>
2025-11-01 15:25:30 +0100

    feat: enhance userId extraction with zod validation in layout load function

--- src/routes/app/users/[slug]/+layout.ts
+++ src/routes/app/users/[slug]/+layout.ts
@@ -1,9 +1,11 @@
+import { z } from 'zod';
+
 import type { LayoutLoad } from './$types';
 
 export const load: LayoutLoad = async ({ parent, url }) => {
 	const parentData = await parent();
-  
-  const userId = url.pathname.match(/\/users\/([^\/\?]+)/)?.[1] || '';
+
+	let userId = z.uuidv4().safeParse(url.pathname.match(/\/users\/([^\/\?]+)/)?.[1] || '').data;
 
 	return {
 		breadcrumbs: [...parentData.breadcrumbs, { name: userId, path: userId }]
