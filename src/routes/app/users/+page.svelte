aeac5eb3363ef0aed7b11464d78403f47b86c59f
c90x <e.0xcc3@gmail.com>
2025-11-06 03:18:15 +0100

    feat: implement user creation functionality with validation and alert dialog

--- src/routes/app/users/+page.svelte
+++ src/routes/app/users/+page.svelte
@@ -1,24 +1,165 @@
 <script lang="ts">
+	import { Check, Plus, UserPlus, X } from 'lucide-svelte';
+
 	import { Button } from '#/ui/button';
-	import { Plus } from 'lucide-svelte';
+	import { Input } from '#/ui/input';
+	import Label from '#/ui/label/label.svelte';
+
+	import * as AlertDialog from '#/ui/alert-dialog';
+	import * as InputGroup from '#/ui/input-group';
 
 	import DataTable from './data-table.svelte';
-	import { getUsers } from './users.remote';
+	import { createUser, getUsers } from './users.remote';
+	import { passwordSchema, usernameSchema } from '@/schemas/auth';
+	import { Spinner } from '#/ui/spinner';
+	import { toast } from 'sonner';
 
 	let page = $state(0);
 
-	let users = $derived(await getUsers({ page }));
+	let createOpen = $state(false);
+
+	let createUsername = $state('');
+	let createUsernameValid = $derived.by(() => {
+		if (createUsername.length === 0) return null;
+		return usernameSchema.safeParse(createUsername).success;
+	});
+
+	let createPassword = $state('');
+	let createPasswordValid = $derived.by(() => {
+		if (createPassword.length === 0) return null;
+		return passwordSchema.safeParse(createPassword).success;
+	});
+
+	let createPasswordConfirm = $state('');
+	let createPasswordConfirmValid = $derived.by(() => {
+		if (createPasswordValid === null || createPasswordValid == false) return null;
+		return createPasswordConfirm === createPassword;
+	});
+
+	function resetInput() {
+		createUsername = '';
+	}
 </script>
 
 <div class="flex flex-wrap gap-2">
-	<Button variant="outline" class="ml-auto">
-		<Plus />
-		Neu erstellen
-	</Button>
+	<AlertDialog.Root
+		bind:open={createOpen}
+		onOpenChange={(o) => {
+			if (o) resetInput();
+		}}
+	>
+		<AlertDialog.Trigger>
+			{#snippet child({ props })}
+				<Button {...props} variant="outline" class="ml-auto">
+					<Plus />
+					Neu erstellen
+				</Button>
+			{/snippet}
+		</AlertDialog.Trigger>
+		<AlertDialog.Content class="gap-6">
+			<AlertDialog.Header>
+				<AlertDialog.Title>Nutzer erstellen</AlertDialog.Title>
+			</AlertDialog.Header>
+
+			<div class="space-y-2">
+				<Label>Username</Label>
+				<InputGroup.Root>
+					<InputGroup.Input
+						bind:value={createUsername}
+						type="text"
+						disabled={!!createUser.pending}
+					/>
+					{#if createUsernameValid !== null}
+						<InputGroup.Addon align="inline-end">
+							{#if createUsernameValid}
+								<Check class="text-green-600 dark:text-green-400" />
+							{:else}
+								<X class="text-red-600 dark:text-red-400" />
+							{/if}
+						</InputGroup.Addon>
+					{/if}
+				</InputGroup.Root>
+			</div>
+
+			<div class="space-y-2">
+				<Label>Passwort</Label>
+				<InputGroup.Root>
+					<InputGroup.Input
+						bind:value={createPassword}
+						type="password"
+						disabled={!!createUser.pending}
+					/>
+					{#if createPasswordValid !== null}
+						<InputGroup.Addon align="inline-end">
+							{#if createPasswordValid}
+								<Check class="text-green-600 dark:text-green-400" />
+							{:else}
+								<X class="text-red-600 dark:text-red-400" />
+							{/if}
+						</InputGroup.Addon>
+					{/if}
+				</InputGroup.Root>
+			</div>
+
+			<div class="space-y-2">
+				<Label>Passwort Wiederholen</Label>
+				<InputGroup.Root>
+					<InputGroup.Input
+						bind:value={createPasswordConfirm}
+						type="password"
+						disabled={!!createUser.pending}
+					/>
+					{#if createPasswordConfirmValid !== null}
+						<InputGroup.Addon align="inline-end">
+							{#if createPasswordConfirmValid}
+								<Check class="text-green-600 dark:text-green-400" />
+							{:else}
+								<X class="text-red-600 dark:text-red-400" />
+							{/if}
+						</InputGroup.Addon>
+					{/if}
+				</InputGroup.Root>
+			</div>
+
+			<AlertDialog.Footer>
+				<AlertDialog.Cancel>Abbrechen</AlertDialog.Cancel>
+				<AlertDialog.Action
+					onclick={async () => {
+						try {
+							await createUser({
+								username: createUsername,
+								password: createPassword
+							});
+
+							toast.success('Nutzer erfolgreich erstellt');
+
+							createOpen = false;
+
+							await getUsers({ page }).refresh();
+						} catch (error) {
+							toast.error(
+								`Fehler beim Erstellen von Nutzer: ${(error as any).body?.message ?? 'Unknown Error'}`
+							);
+						}
+					}}
+					disabled={!!createUser.pending}
+				>
+					{#if !!createUser.pending}
+						<Spinner />
+					{:else}
+						<UserPlus />
+					{/if}
+					Erstellen
+				</AlertDialog.Action>
+			</AlertDialog.Footer>
+		</AlertDialog.Content>
+	</AlertDialog.Root>
 </div>
 
 <div class="my-2 space-y-4">
-	{#if users}
-		<DataTable users={users.page} />
-	{/if}
+	{#await await getUsers({ page }) then users}
+		{#if users}
+			<DataTable users={users.page} />
+		{/if}
+	{/await}
 </div>
--- src/routes/app/users/users.remote.ts
+++ src/routes/app/users/users.remote.ts
@@ -5,6 +5,7 @@ import { withAuthClient } from '@/server/auth';
 import * as sdk from '@/backend/sdk.gen';
 
 import { z } from 'zod';
+import { passwordSchema, usernameSchema } from '@/schemas/auth';
 
 export const getUsers = query(
 	z.object({
@@ -34,3 +35,25 @@ export const deleteUser = command(z.uuidv4(), async (id) => {
 		error(result.response.status, result.error.detail?.at(0)?.msg ?? 'Unknown Error');
 	}
 });
+
+export const createUser = command(
+	z.object({
+		username: usernameSchema,
+		password: passwordSchema
+	}),
+	async (body) => {
+		const { client } = withAuthClient({ superuser: true });
+
+		const res = await sdk.createUser({
+			client,
+			body
+		});
+
+		if (res.error) {
+			error(
+				res.response.status,
+				res.error.detail?.at(0)?.msg ?? (res.error.detail as string | undefined) ?? 'Unknown Error'
+			);
+		}
+	}
+);
