e7f08034db0b626e5d3e16765e44dac1324ec300
c90x <e.0xcc3@gmail.com>
2025-11-06 02:34:58 +0100

    feat: implement Gleitzeit warning input handling and server update functionality

--- src/routes/app/settings/+page.svelte
+++ src/routes/app/settings/+page.svelte
@@ -1,9 +1,71 @@
 <script lang="ts">
+	import Button from '#/ui/button/button.svelte';
+	import { Input } from '#/ui/input';
+	import { Label } from '#/ui/label';
+	import { onMount } from 'svelte';
 	import type { PageData } from './$types';
+	import { getServerStore, setServerStoreGleitzeitwarnung } from './settings.remote';
+	import { toast } from 'sonner';
 
 	let props: { data: PageData } = $props();
-	let {
-		data: { user, accessToken }
-	} = props;
+
+	let valueYellow: number | undefined = $state(undefined);
+	let valueRed: number | undefined = $state(undefined);
 </script>
 
+<div class="max-w-[20rem] space-y-6">
+	{#await getServerStore() then store}
+		{#if store}
+			<div class="space-y-2">
+				<Label>Gleitzeit Warnung Gelb</Label>
+				<Input
+					value={store.gleitzeit_warnung_gelb !== undefined ? store.gleitzeit_warnung_gelb : '-'}
+					type="text"
+					readonly
+					disabled
+				/>
+			</div>
+
+			<div class="space-y-2">
+				<Label>Gleitzeit Warnung Rot</Label>
+				<Input
+					value={store.gleitzeit_warnung_rot !== undefined ? store.gleitzeit_warnung_rot : '-'}
+					type="text"
+					readonly
+					disabled
+				/>
+			</div>
+		{/if}
+	{/await}
+
+	<div class="space-y-2">
+		<Label>Neu Gleitzeit Warnung Gelb</Label>
+		<Input bind:value={valueYellow} type="text" />
+	</div>
+
+	<div class="space-y-2">
+		<Label>Neu Gleitzeit Warnung Rot</Label>
+		<Input bind:value={valueRed} type="text" />
+	</div>
+
+	<Button
+		onclick={async () => {
+			try {
+				await setServerStoreGleitzeitwarnung({
+					s1: valueYellow as any,
+					s2: valueRed as any
+				});
+
+				toast.success('Abwesendheitseintrag erfolgreich erstellt');
+
+				await getServerStore().refresh();
+			} catch (error) {
+				toast.error(
+					`Fehler beim Erstellen vom Abwesendheitseintrag: ${(error as any).body?.message ?? 'Unknown Error'}`
+				);
+			}
+		}}
+	>
+		Speichern
+	</Button>
+</div>
--- src/routes/app/settings/settings.remote.ts
+++ src/routes/app/settings/settings.remote.ts
@@ -1,4 +1,6 @@
-import { query } from '$app/server';
+import { z } from 'zod';
+
+import { command, query } from '$app/server';
 import { withAuthClient } from '@/server/auth';
 
 import * as sdk from '@/backend/sdk.gen';
@@ -15,3 +17,35 @@ export const getServerStore = query(async () => {
 
 	return res.data;
 });
+
+export const setServerStoreGleitzeitwarnung = command(
+	z.object({
+		s1: z.int().min(1).optional(),
+		s2: z.int().min(1).optional()
+	}),
+	async (data) => {
+		const { client } = withAuthClient();
+
+		if (
+			(data.s1 === undefined && data.s2 !== undefined) ||
+			(data.s1 !== undefined && data.s2 === undefined)
+		)
+			error(400, 'Bad request');
+
+		const res = await sdk.patchServerStoreGleitzeitWarnungen({
+			client,
+			query: {
+				gleitzeit_warnung_gelb: data.s1,
+				gleitzeit_warnung_rot: data.s2
+			}
+		});
+
+		if (res.error) {
+			error(res.response.status, JSON.stringify(res.error));
+		}
+
+		getServerStore().set(res.data);
+
+		return res.data;
+	}
+);
